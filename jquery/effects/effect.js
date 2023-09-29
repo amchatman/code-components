bba2b7b65972e5864bf9ebb8c09ef28398cfd829
Ashley Chatman <ashchat2008@gmail.com>
2023-09-29 13:57:24 -0400

    Move files -Delete style and jquery code from html file -Add code to effect.css -Add code to effect.js

---
+++ jquery/effects/effect.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+    $("#fadeIn").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });
+    $("#fadeOut").click(function(){
+        $("#div4").fadeOut();
+        $("#div5").fadeOut("slow");
+        $("#div6").fadeOut(3000);
+    });
+    $("#fadeToggle").click(function(){
+        $("#div7").fadeToggle();
+        $("#div8").fadeToggle("slow");
+        $("#div9").fadeToggle(3000);
+    });
+    $("#fadeTo").click(function(){
+        $("#div10").fadeTo("slow",0.15);
+        $("#div11").fadeTo("slow",0.4);
+        $("#div11").fadeTo("slow",0.7);
+    });
+    $("#down").click(function(){
+        $("#panel").slideDown();
+    });
+});
