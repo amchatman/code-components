1a0385d929d742388a7f783e7c9a8c8c0317d504
Ashley Chatman <ashchat2008@gmail.com>
2023-11-08 13:32:10 -0500

    React setup -Finished React setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './reactNotes.js';
 
 function App() {
   return (
---
+++ my-app/src/reactNotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function reactNotes (){
+    return(
+        <div>
+            <h1>This is a home page</h1>
+        </div>
+    );
+}
+export default reactNotes;
